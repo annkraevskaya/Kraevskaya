05a58268557c1adb2ca1dc4e5edd16139a10b35f
Anna <kraevskayaannaa@mail.ru>
2025-06-22 21:05:01 +0300

    правки по ООП ч1

--- 9_OOPpom1.py
+++ 9_OOPpom1.py
@@ -1,16 +1,22 @@
 
-  class LibraryBook:           #создание класса (шаблон)
-    _LIBRARY_NAME ="Central City Library"    #инициализация общего атрибута
+class LibraryBook:           #создание класса (шаблон)
 
-    def __init__(self, title,author):     #создание констурктора
+    _LIBRARY_NAME = "Central City Library"    #инициализация общего атрибута
+
+    def __init__(self, title, author):     #создание констурктора
         self.title = title
         self.author = author
 
     def info(self):        #создание метода info для взаимодействия
         return f"{self.title} by {self.author} - kept in {self._LIBRARY_NAME}"
 
-book=LibraryBook("The Green Mile", "Stephen King")    #создание новой книги
+
+
+
+book = LibraryBook("The Green Mile", "Stephen King")    #создание новой книги
 print(book.info())
 
-LibraryBook._LIBRARY_NAME="Downtown Branch"    #замена общего атрибута
+LibraryBook._LIBRARY_NAME = "Downtown Branch"    #замена общего атрибута
 print(book.info())
+
+print(book.author)
